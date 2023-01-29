a12dd1c1bd618552eeed1605516b53ff4f12fd6e
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-01-29 15:59:18 +0100

    Button-Evt-Handler funktonieren jetzt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,20 +6,29 @@ import { Component } from '@angular/core'
 })
 export class HomeComponent {
 
+  /** Member-Variable, wird mit Interpolation auf UI dargestellt. */
   public zaehlerwert = 0;
 
   constructor() {
-    // Use the component constructor to inject providers.
+    console.log("Konstruktor wurde aufgerufen.");
   }
 
+  /**
+   * Event-Händler-Methode für Plus-Button.
+   */
   public onPlusButton() {
 
     this.zaehlerwert++;
+    console.log(`Zähler-Wert nach Plus: ${this.zaehlerwert}`);
   }
 
+  /**
+   * Event-Handler-Methode für Minus-Button.
+   */
   public onMinusButton() {
 
     this.zaehlerwert--;
+    console.log(`Zähler-Wert nach Minus: ${this.zaehlerwert}`);
   }
 
 }
