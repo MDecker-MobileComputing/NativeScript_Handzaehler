732093a3ac13ceeb4662736ad266fecc2275e3f1
MDecker-MobileComputer <mide_42b@yahoo.de>
2023-01-29 13:59:35 +0100

    Event-Handler-Methoden fuer Button dazu, werden aber nicht aufgerufen :-(

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'Home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+
+  public zaehlerwert = 0;
+
   constructor() {
     // Use the component constructor to inject providers.
   }
 
-  ngOnInit(): void {
-    // Init your component properties here.
+  public onPlusButton() {
+
+    this.zaehlerwert++;
   }
+
+  public onMinusButton() {
+
+    this.zaehlerwert--;
+  }
+
 }
